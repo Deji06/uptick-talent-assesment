973431b7a2de15062445f43a1df83de2f9ed9b53
Oladeji <olawunioladeji06@gmail.com>
2025-06-04 12:25:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,250 @@
-import logo from './logo.svg';
-import './App.css';
+import { FaSearch } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import MovieList from "./component/MovieList";
+import axios from "axios";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function App() {
+  const [changeWidth, setchangeWidth] = useState(false);
+  const [input, setInput] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [searchedMovie, setSearchedMovie] = useState([]);
+  const [displayByGenre, setDisplayByGenre] = useState([]);
+  const [movieList, setMovieList] = useState([]);
+  const [genreChecker, setGenreChecker] = useState("");
+  const [searchMoviesFrom, setSearchMoviesFrom] = useState("");
+  const [searchMoviesTo, setSearchMoviesTo] = useState("");
+  const [displayMenu, setDisplayMenu] = useState(false);
+
+  // search function
+  useEffect(() => {
+    const delaySearch = setTimeout(() => {
+      if (input.trim().length > 0) {
+        searchResult(input);
+      } else {
+        setSearchedMovie([]);
+      }
+    }, 500);
+    return () => clearTimeout(delaySearch);
+  }, [input]);
+
+  const searchResult = async (query) => {
+    setLoading(true);
+    setError(null);
+    const url = "https://api.themoviedb.org/3/search/movie";
+    const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+    try {
+      const response = await axios.get(url, {
+        params: {
+          api_key: API_KEY,
+          query: query,
+          page: 1,
+          language: "en-US",
+        },
+      });
+      const data = response.data.results;
+      setSearchedMovie(data);
+      // setInput(searchedMovie)
+      console.log("search result", data);
+    } catch (error) {
+      console.error("error fetching search result", error);
+      setError("Failed to fetch movie");
+      setSearchedMovie([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // filtering function
+  useEffect(() => {
+    let moviesToRender = [];
+    const filteredMovies = movieList.filter((filteredMovies) => {
+      if (!genreChecker || genreChecker === "Genre") {
+        return true;
+      }
+      // converting movielist genre id to generic genretype object
+      const movieGenreNames = filteredMovies.genre_ids.map(
+        (id) => genreType[id]
+      );
+      console.log("filterresult", movieGenreNames);
+      return movieGenreNames.includes(genreChecker);
+    });
+
+    if (input.trim().length > 0) {
+      moviesToRender = searchedMovie;
+    } else {
+      moviesToRender = filteredMovies;
+    }
+    console.log("filteredmoviesdata:", filteredMovies);
+    setDisplayByGenre(moviesToRender);
+  }, [genreChecker, movieList, searchedMovie, input]);
+
+  // popular movies display
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+    const fetchMovieList = async () => {
+      const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+      try {
+        const url = "https://api.themoviedb.org/3/movie/popular";
+        const res = await axios.get(url, {
+          params: {
+            api_key: API_KEY,
+            language: "en-US",
+            page: 1,
+          },
+        });
+        setMovieList(res.data.results);
+        // console.log("fetched data:", res.data.results);
+      } catch (error) {
+        console.log("error fetching movies...", error);
+      }
+    };
+    fetchMovieList();
+  }, []);
+
+  // sort movies by date
+  useEffect(() => {
+    const sortMoviesByDate = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const url = "https://api.themoviedb.org/3/discover/movie";
+        const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+        const params = {
+          api_key: API_KEY,
+          language: "en-US",
+          page: 1,
+          sort_by: "popularity.desc",
+        };
+        if (searchMoviesFrom) {
+          params["primary_release_date.gte"] = searchMoviesFrom;
+        }
+        if (searchMoviesTo) {
+          params["primary_release_date.lte"] = searchMoviesTo;
+        }
+
+        const res = await axios.get(url, { params });
+        setMovieList(res.data.results);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+        setError("Failed to load movies.");
+      } finally {
+        setLoading(false);
+      }
+    };
+    sortMoviesByDate();
+  }, [searchMoviesFrom, searchMoviesTo]);
+
+  // generic genretype
+  const genreType = {
+    27: "Horror",
+    28: "Action",
+    12: "Adventure",
+    16: "Animation",
+    35: "Comedy",
+    80: "Crime",
+    99: "Documentary",
+    18: "Drama",
+    10751: "Family",
+    10752: "War",
+    14: "Fantasy",
+    36: "History",
+    10402: "Music",
+    53: "Thriller",
+    37: "Western",
+    10770: "Tv Movie",
+    878: "Science Fiction",
+    9648: "Mistery",
+    10749: "Romance",
+  };
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    setInput("");
+  };
+
+  // handle hamburgger
+
+  const handleUserInput = (e) => {
+    setInput(e.target.value);
+    console.log(input);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="flex sm:flex-row flex-col gap-y-5 justify-between sm:items-center pt-10">
+        <div className="flex justify-between items-center">
+          <div className="text-white sm:px-5 text-[30px] ml-5 uppercase">
+            movie <span className="text-red-900">app</span>{" "}
+          </div>
+          <GiHamburgerMenu
+            onClick={() => setDisplayMenu(!displayMenu)}
+            className="text-red-900 text-[30px] mr-5 sm:hidden"
+          />
+        </div>
+        {/* display on small screen */}
+        {displayMenu && (
+          <form
+            action=""
+            onSubmit={handleForm}
+            className={` transition-all duration-300 border-2 w-[80%] ml-5 ${
+              changeWidth ? "sm:w-[60%] && border-red-900" : "sm:w-[20%]"
+            } mr-10 rounded`}
+          >
+            <div className="flex items-center gap-x-2 py-1 border-none  bg-white rounded ">
+              <FaSearch className="text-[20px] mx-4 bg-white text-[#CCCDDE] " />
+              <input
+                value={input}
+                onChange={handleUserInput}
+                type="text"
+                placeholder="search"
+                className="w-full  outline-none border-none"
+                onFocus={() => setchangeWidth(!changeWidth)}
+                onBlur={() => setchangeWidth(false)}
+              />
+            </div>
+          </form>
+        )}
+        {/* big screen display */}
+        <form
+          action=""
+          onSubmit={handleForm}
+          className={` transition-all duration-300 border-2 bg-white  ml-5 ${
+            changeWidth ? "sm:w-[60%] && border-red-900" : "sm:w-[20%]"
+          } mr-10 rounded hidden sm:flex md:hidden`}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="flex items-center gap-x-2 py-1 border-none  bg-white rounded ">
+            <FaSearch className="text-[20px] mx-4 bg-white text-[#CCCDDE] " />
+            <input
+              value={input}
+              onChange={handleUserInput}
+              type="text"
+              placeholder="search"
+              className="w-full  outline-none border-none"
+              onFocus={() => setchangeWidth(!changeWidth)}
+              onBlur={() => setchangeWidth(false)}
+            />
+          </div>
+        </form>
+      </div>
+      <MovieList
+        // setSearchedMovie={setSearchedMovie}
+        // searchedMovie={searchedMovie}
+        displayMenu={displayMenu}
+        displayByGenre={displayByGenre}
+        setDisplayByGenre={setDisplayByGenre}
+        loading={loading}
+        error={error}
+        input={input}
+        genreType={genreType}
+        setGenreChecker={setGenreChecker}
+        searchMoviesFrom={searchMoviesFrom}
+        searchMoviesTo={searchMoviesTo}
+        setSearchMoviesFrom={setSearchMoviesFrom}
+        setSearchMoviesTo={setSearchMoviesTo}
+      />
+    </>
   );
 }
 
---
+++ src/component/MovieList.jsx
@@ -0,0 +1,219 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const MovieList = ({
+  // setSearchedMovie,
+  genreType,
+  // searchedMovie,
+  loading,
+  error,
+  input,
+  // setDisplayByGenre,
+  displayByGenre,
+  displayMenu,
+  // setMovieList,
+  setGenreChecker,
+  genreChecker,
+  setSearchMoviesFrom,
+  setSearchMoviesTo,
+  searchMoviesFrom,
+  searchMoviesTo,
+}) => {
+  const [genre, setGenre] = useState([]);
+  useEffect(() => {
+    const getGenryCategory = async () => {
+      const url = "https://api.themoviedb.org/3/genre/movie/list";
+      const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+      try {
+        const res = await axios.get(url, {
+          params: {
+            api_key: API_KEY,
+            language: "en-US",
+          },
+        });
+        setGenre(res.data.genres);
+        setGenreChecker(res.data.genres.name);
+        console.log("selected genre:", res.data.genres.name);
+
+        console.log("genre data:", res.data);
+      } catch (error) {
+        console.log("error fetching genre:", error);
+      }
+    };
+    getGenryCategory();
+  }, []);
+  // const[displayByGenre, setDisplayByGenre] = useState([])
+
+  if (loading) {
+    return (
+      <p className="text-red-900 flex justify-center mt-10">loading....</p>
+    );
+  }
+  if (error) {
+    return <p className="text-red-900">Error: {error}</p>;
+  }
+
+  // Handle case where no movies are found after filtering/searching
+  if (displayByGenre.length === 0) {
+    return (
+      <p style={{ color: "black", textAlign: "center", marginTop: "20px" }}>
+        No movies found for your selection.
+      </p>
+    );
+  }
+
+  const handleGenre = (e) => {
+    setGenreChecker(e.target.value);
+    console.log(e.target.value);
+  };
+
+  // tmdbApi = '187f3133b748cacd5f072c3a3a971e7c'
+  // eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxODdmMzEzM2I3NDhjYWNkNWYwNzJjM2EzYTk3MWU3YyIsIm5iZiI6MTcwMDQ0MTE4OC42MjcsInN1YiI6IjY1NWFhYzY0YjU0MDAyMTRjZjNhYWM5ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7NWmLY7s7qKPqeV035LscjqNsJRHlV_8ypCnChOYzUA
+
+  return (
+    <div className="mx-5 sm:mt-5 h-auto">
+      {/* mobile display */}
+      {displayMenu && (
+        <div className="flex sm:flex-row flex-col sm:items-center justify-between transition-all duration-300">
+          <div className="flex items-center gap-x-5 mt-5">
+            <p className="sm:px-5 text-white sm:text-[30px] text-[20px]">
+              latest releases
+            </p>
+            {/* genre options */}
+            <select
+              name="GENRE"
+              id=""
+              value={genreChecker}
+              className="bg-black w-[20%] text-red-900 text-[20px] border-none outline-none "
+              onChange={handleGenre}
+            >
+              <option value="" selected className="text-gray-400">
+                Genre
+              </option>
+              {genre.map((genreItem) => (
+                <option
+                  key={genreItem.id}
+                  value={genreItem.name}
+                  className="text-white flex flex-col cursor-pointer"
+                >
+                  {genreItem.name}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="flex  gap-x-2 mr-5 mt-5 sm:mt-0">
+            <div className="flex items-center space-x-2">
+              <label htmlFor="" className="text-red-900">
+                From:
+              </label>
+              <input
+                type="date"
+                value={searchMoviesFrom}
+                onChange={(e) => setSearchMoviesFrom(e.target.value)}
+              />
+            </div>
+
+            <div className="flex items-center space-x-2">
+              <label htmlFor="" className="text-red-900">
+                To:
+              </label>
+              <input
+                type="date"
+                value={searchMoviesTo}
+                onChange={(e) => setSearchMoviesTo(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+      )}
+      {/* big screen display */}
+      <div className="sm:flex-row flex-col sm:items-center justify-between transition-all duration-300 hidden sm:flex md:hidden">
+        <div className="flex items-center gap-x-5 mt-5">
+          <p className="sm:px-5 text-white sm:text-[30px] text-[20px]">
+            latest releases
+          </p>
+          {/* genre options */}
+          <select
+            name="GENRE"
+            id=""
+            value={genreChecker}
+            className="bg-black w-[20%] text-red-900 text-[20px] border-none outline-none "
+            onChange={handleGenre}
+          >
+            <option value="" selected className="text-gray-400">
+              Genre
+            </option>
+            {genre.map((genreItem) => (
+              <option
+                key={genreItem.id}
+                value={genreItem.name}
+                className="text-white flex flex-col cursor-pointer"
+              >
+                {genreItem.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="flex  gap-x-2 mr-5 mt-5 sm:mt-0">
+          <div className="flex items-center space-x-2">
+            <label htmlFor="" className="text-red-900">
+              From:
+            </label>
+            <input
+              type="date"
+              value={searchMoviesFrom}
+              onChange={(e) => setSearchMoviesFrom(e.target.value)}
+            />
+          </div>
+
+          <div className="flex items-center space-x-2">
+            <label htmlFor="" className="text-red-900">
+              To:
+            </label>
+            <input
+              type="date"
+              value={searchMoviesTo}
+              onChange={(e) => setSearchMoviesTo(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* movie Lisst */}
+      <div className="grid sm:grid-cols-3 gap-x-3 gap-y-5 pb-10 mt-10 sm:mt5">
+        {displayByGenre.map((movie) => (
+          <div key={movie.id} className="w-fit px-5 space-y-3">
+            <img
+              className="w-[100%]"
+              src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+              alt="movie jpg"
+            />
+            <h2 className="text-white text-[20px]">
+              <span className="text-red-900 text-[15px]">Title: </span>
+              {movie.title}
+            </h2>
+            <p className="text-white ">
+              {" "}
+              <span className="text-red-900">Release date:</span>{" "}
+              {movie.release_date}
+            </p>
+            <h2 className="text-red-900 w-fit text-[13px]">{movie.overview}</h2>
+            <p className="text-white">
+              <span className="text-red-900">Genre:</span>{" "}
+              {movie.genre_ids.map((id) => genreType[id]).join(", ")}
+            </p>
+          </div>
+        ))}
+      </div>
+      {displayByGenre.length === 0 && genreChecker !== "Genre" && (
+        <p className="text-red-900 flex justify-center text-[30px]">
+          sorry!! no movie to display in this category....
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default MovieList;
